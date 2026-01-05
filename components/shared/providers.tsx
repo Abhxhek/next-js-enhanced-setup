05dd93bbf9d6de77e945bdb76ec3231152feb59c
Abhxhek <shriabhishek929@gmail.com>
2026-01-05 14:15:51 +0530

    added zustand and tanstack query

--- components/shared/providers.tsx
+++ components/shared/providers.tsx
@@ -4,8 +4,10 @@ import { Toaster } from "@/components/ui/sonner";
 import { Provider } from "react-redux";
 import { useEffect, useState } from "react";
 import store from "@/redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+    const [queryClient] = useState(new QueryClient());
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
@@ -15,15 +17,19 @@ export default function Providers({ children }: { children: React.ReactNode }) {
     if (!mounted) {
         return (
             <Provider store={store}>
-                {children}
+                <QueryClientProvider client={queryClient}>
+                    {children}
+                </QueryClientProvider>
             </Provider>
         );
     }
 
     return (
         <Provider store={store}>
-            {children}
-            <Toaster />
+            <QueryClientProvider client={queryClient}>
+                {children}
+                <Toaster />
+            </QueryClientProvider>
         </Provider>
     );
 }
